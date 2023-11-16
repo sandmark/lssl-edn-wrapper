c5cd421537a29708b1fa00aeddfd2adfca7b207d
sandmark <sandmark.m@gmail.com>
2023-11-16 20:36:31 +0900

    Fix cider couldn't resolve refresh function

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
 ((clojure-mode . ((cider-clojure-cli-aliases . ":dev")))
- (nil . ((cider-ns-refresh-before-fn . "")
-         (cider-ns-refresh-after-fn  . ""))))
+ (nil . ((cider-ns-refresh-before-fn . nil)
+         (cider-ns-refresh-after-fn  . nil))))
